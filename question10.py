fb11f18a53c0c01c99bf0fab111853229900ea7a
Svetlana Cervantes <114374105+svetacervantes@users.noreply.github.com>
2022-10-02 16:46:12 -0700

    adding question 10

---
+++ question10.py
@@ -0,0 +1,6 @@
+#Fasten (verb) your seatbelts (noun). It's going to be a bumpy(adjective) ride (noun).
+verb=input("give me a verb:  ")
+noun1=input("give me a noun:  ")
+adjective=input("give me an adjective:  ")
+noun2=input("give me a noun:  ")
+print("" + verb + " your " + noun1 + ". It's going to be a " + adjective +" " + noun2 + " .")
