268108063be166d43e6cf16f45f3a6ad1dff7a27
Svetlana Cervantes <114374105+svetacervantes@users.noreply.github.com>
2022-10-01 20:18:09 -0700

    answer to question 9

---
+++ question9.py
@@ -0,0 +1,6 @@
+# "The quick, adjective noun past-tense verb over the lazy noun."
+adjective=input("give me an adjective: ")
+noun1=input("give me a noun:  ")
+verb=input("give me a verb:  ")
+noun2=input("give me a noun:  ")
+print("The quick, " + adjective + " " + noun1 +" " + verb +" over the lazy "+ noun2 +".")
